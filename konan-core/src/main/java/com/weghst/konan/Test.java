4257ee3ec7ade417dea20c1056fce525577ccf2a
kevin <kevinz@weghst.com>
2016-10-17 19:00:09 +0800

    测试

---
+++ konan-core/src/main/java/com/weghst/konan/Test.java
@@ -0,0 +1,10 @@
+package com.weghst.konan;
+
+/**
+ * Created by kevin on 16/10/15.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(System.getenv());
+    }
+}
